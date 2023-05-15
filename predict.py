82f4a2575359a490eb7f4a1ee981d5f4e30720f9
Muhammad Jaber <skmdjaber@gmail.com>
2023-05-15 23:07:41 +0600

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import joblib
 
-# Load the trained model from disk
-rfc = joblib.load('models/random_forest.pkl')
+rfc = joblib.load('random_forest.pkl')
 
-# Load the new data into a pandas DataFrame
 new_data = pd.DataFrame({
     'level': [2],
     'age': [35],
@@ -13,7 +11,6 @@ new_data = pd.DataFrame({
     'debt_ratio': [0.25]
 })
 
-# Make predictions on the new data
 predictions = rfc.predict(new_data)
 
 print('Predicted Reason:', predictions[0][0])
